57783ba18b5886bae5340f7ed51911b06eb9548a
ec975 <ec975@cornell.edu>
2026-03-22 18:48:47 -0400

    added new graphs page
    Created new graphs page with labels for each graph and connected it to the home screen.

---
+++ app/src/main/java/com/example/aguadatos_v2/ui/screens/Graph.kt
@@ -0,0 +1,162 @@
+package com.example.aguadatos_v2.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.aguadatos_v2.R
+import com.example.aguadatos_v2.ui.components.BottomNavigationBar
+import com.example.aguadatos_v2.ui.components.GraphButton
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewGraphScreen() {
+    Graph({}, {}, {}, {})
+}
+@Composable
+fun Graph (
+    // will make all of the graph functions soon
+    onBackClick: () -> Unit,
+    onHomeClick: () -> Unit,
+    onRecordsClick: () -> Unit,
+    onProfileClick: () -> Unit,
+) {
+
+    val provider = GoogleFont.Provider(
+        providerAuthority = "com.google.android.gms.fonts",
+        providerPackage = "com.google.android.gms",
+        certificates = R.array.com_google_android_gms_fonts_certs
+    )
+    val fontName = GoogleFont("Lato")
+    val fontFamily = FontFamily(
+        Font(
+            googleFont = fontName,
+            fontProvider = provider,
+        )
+    )
+
+    val labels = listOf(
+        "PLANT FLOW",
+        "RAW WATER",
+        "COAG. DOSE",
+        "CHLORINE DOSE",
+        "FILTER TURBID.",
+        "CLARIFIED TURBID."
+    )
+
+    Scaffold(
+        containerColor = Color(0xffdbecff),
+        bottomBar = {
+            BottomNavigationBar(
+                onHomeClick = onHomeClick,
+                onRecordsClick = onRecordsClick,
+                onGraphsClick = {},
+                onProfileClick = onProfileClick,
+                currentScreen = "Graph"
+            )
+        }
+
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, bottom = 24.dp)
+                    .padding(horizontal = 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                //back arrow
+                Image(
+                    painter = painterResource(id = R.drawable.back_arrow),
+                    contentDescription = stringResource(R.string.back),
+                    modifier = Modifier
+                        .size(28.dp)
+                        .clickable { onBackClick() }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text (
+                    text = "GRAPHS",
+                    fontSize = 24.sp,
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.size(28.dp))
+            }
+            // columns
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                for (i in labels.indices step 3) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 5.dp)
+                            .height(55.dp),
+                        horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        // First button in the row
+                        GraphButton(
+                            text = labels[i],
+                            onClick = {},
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight()
+                        )
+                        // Second button in row
+                        if (i + 1 < labels.size) {
+                            GraphButton(
+                                text = labels[i + 1],
+                                onClick = {},
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                            )
+                        }
+
+                        if (i + 2 < labels.size) {
+                            GraphButton(
+                                text = labels[i+2],
+                                onClick = {},
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
