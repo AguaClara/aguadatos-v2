2fa2a539fa20905ba491d9449b428a223fb90fee
ec975 <ec975@cornell.edu>
2026-05-03 16:31:41 -0400

    Fixed titles and added units

--- app/src/main/java/com/example/aguadatos_v2/ui/components/DataGraph.kt
+++ app/src/main/java/com/example/aguadatos_v2/ui/components/DataGraph.kt
@@ -35,6 +35,22 @@ fun DataGraph(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) 
         fontProvider = provider,
     )
     )
+    var title = text;
+    when (text) {
+        "Coag. Dose" -> title = "Coagulant Dose"
+        "Filter Turbid." -> title = "Filtered Turbidity"
+        "Clarified Turbid." -> title = "Clarified Turbidity"
+    }
+    var unit = "NTU"
+    when (text) {
+        "Plant Flow" -> unit = " (Unknown)"
+        "Raw Water" -> unit = " (NTU)"
+        "Coag. Dose" -> unit = " (mg/L)"
+        "Chlorine Dose" -> unit = " (mg/L)"
+        "Filter Turbid." -> unit = " (mg/L)"
+        "Clarified Turbid." -> unit = " (mg/L)"
+    }
+    
     Box (
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -49,7 +65,7 @@ fun DataGraph(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) 
             contentAlignment = Alignment.TopStart
         ) {
             Text(
-                text = text,
+                text = title + unit,
                 color = Color.Black,
                 modifier = Modifier.padding(10.dp),
                 fontFamily = fontFamily,
